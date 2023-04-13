c2b47865a22efa23d5d4ec03dd30e315fd0172af
Ance <117335537+AnceYin@users.noreply.github.com>
2023-04-13 14:18:27 +0800

    更新 main.cpp

--- main.cpp
+++ main.cpp
@@ -1,80 +1,78 @@
 #include "architechture.h"
-//负责人：asd
-//画三个地图
-const vector<vector<int>>level_1;
-const vector<vector<int>>level_2;
-const vector<vector<int>>level_3;
-vector<vector<int>>map = {
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+// 负责人：asd
+// 画三个地图
+const vector<vector<int>> level_1;
+const vector<vector<int>> level_2;
+const vector<vector<int>> level_3;
+vector<vector<int>> map = {
+    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 };
 
 int main() {
-	//音乐音量，音效音量
-	int music_volume, sound_volume;
-	//包含游戏中所有敌对单位的链表
-	list<Enemy>enemy_list;
-	//包含游戏中所有子弹的链表
-	list<Bullet>bullet_list;
-	//玩家
-	Player player({0,0},{0,0});
-	//基地位置
-	Position base_position;
-	//游玩时间
-	time_t play_time;
-	//主界面的玩家选择
-	int choice;
-	//关卡
-	int level;
-	while (1) {
-		//是否存在有效存档
-		bool has_data = ReadData(level, enemy_list, bullet_list, player, base_position, map, play_time);
-		choice=MainMenu();
-		switch (choice)
-		{
-		case 1:
-			if (has_data&&ContinueGame()) {
-				NewGame(level,enemy_list,bullet_list,player, base_position, map,play_time);
-				break;
-			}
-			level=LevelSelect();
-			if (!level)break;
-			InitMap(level, enemy_list, bullet_list, player, base_position, map, play_time);
-			NewGame(level, enemy_list, bullet_list, player, base_position, map, play_time);
-			break;
-		case 2:
-			Settings(music_volume, sound_volume);
-			break;
-		case 3:
-			Help();
-			break;
-		case 4:
-			Developers();
-			break;
-		case 5:
-			exit(0);
-		default:
-			break;
-		}
-	}
-	
-
+    // 音乐音量，音效音量
+    int music_volume, sound_volume;
+    // 包含游戏中所有敌对单位的链表
+    list<Enemy> enemy_list;
+    // 包含游戏中所有子弹的链表
+    list<Bullet> bullet_list;
+    // 玩家
+    Player player({0, 0}, {0, 0});
+    // 基地位置
+    Position base_position;
+    // 游玩时间
+    time_t play_time;
+    // 主界面的玩家选择
+    int choice;
+    // 关卡
+    int level;
+    while (1) {
+        // 是否存在有效存档
+        bool has_data = ReadData(level, enemy_list, bullet_list, player, base_position, map, play_time);
+        choice = MainMenu();
+        switch (choice) {
+            case 1:
+                if (has_data && ContinueGame()) {
+                    NewGame(level, enemy_list, bullet_list, player, base_position, map, play_time);
+                    break;
+                }
+                level = LevelSelect();
+                if (!level)
+                    break;
+                InitMap(level, enemy_list, bullet_list, player, base_position, map, play_time);
+                NewGame(level, enemy_list, bullet_list, player, base_position, map, play_time);
+                break;
+            case 2:
+                Settings(music_volume, sound_volume);
+                break;
+            case 3:
+                Help();
+                break;
+            case 4:
+                Developers();
+                break;
+            case 5:
+                exit(0);
+            default:
+                break;
+        }
+    }
 }
