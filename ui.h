9c82caaafd91c252fa64b819b5c15798cc5beae2
Ance <117335537+AnceYin@users.noreply.github.com>
2023-04-19 15:42:52 +0800

    add music class

--- ui.h
+++ ui.h
@@ -1,19 +1,21 @@
-﻿
 #pragma once
 #include <easyx.h>
+#include <mmsystem.h>
 #include <list>
 #include <string>
 #include <iostream>
+#pragma comment(lib, "winmm.lib")
 using std::cout;
 using std::endl;
+
 class image {
 private:
     std::string path_;
     int x_, y_;
     IMAGE img_;
-
+    
 public:
-    image(std::string path, int x = 0, int y = 0)  // ������ɫ��ʽ
+    image(std::string path, int x = 0, int y = 0)
         : path_(path), x_(x), y_(y) {
         loadimage(&img_, path_.c_str());
     };
@@ -33,18 +35,18 @@ public:
         : x_(x), y_(y), w_(w), h_(h), text_(text), line_color_(color), solid_(solid), full_color_(full_color) {}
     void draw() {
         if (solid_) {
-            setfillcolor(full_color_);                // �����ɫ
-            setlinecolor(line_color_);                // ������ɫ
-            fillrectangle(x_, y_, x_ + w_, y_ + h_);  // ������
+            setfillcolor(full_color_);
+            setlinecolor(line_color_);
+            fillrectangle(x_, y_, x_ + w_, y_ + h_);
         }
         else {
-            setlinecolor(line_color_);            // ������ɫ
-            rectangle(x_, y_, x_ + w_, y_ + h_);  // ������
+            setlinecolor(line_color_);
+            rectangle(x_, y_, x_ + w_, y_ + h_);
         }
         if (!(text_ == "")) {
             setbkmode(TRANSPARENT);
-            settextcolor(line_color_);    // ������ɫ
-            settextstyle(20, 0, "����");  // ������ʽ
+            settextcolor(line_color_);
+            settextstyle(20, 0, "黑体");
             outtextxy(x_ + w_ / 2 - textwidth(text_.c_str()) / 2, y_ + h_ / 2 - textheight(text_.c_str()) / 2, text_.c_str());
         }
     }
@@ -54,31 +56,58 @@ public:
     int get_h() const { return h_; }
 };
 
+class music {
+   private:
+    std::string path_;
+    int volume_;
+
+   public:
+    music(std::string path, int volume = 100) : path_(path), volume_(volume) {
+        std::string cmd = "open " + path_;
+        mciSendString(cmd.c_str(), NULL, 0, NULL);
+        set_volume(volume_);
+    };
+    void play(bool loop = false) {
+        std::string cmd = "play " + path_;
+        if (loop)
+            cmd += " repeat";
+        mciSendString(cmd.c_str(), NULL, 0, NULL);
+    };
+    void stop() {
+        std::string cmd = "stop " + path_;
+        mciSendString(cmd.c_str(), NULL, 0, NULL);
+    };
+    void set_volume(int volume) {
+        volume_ = volume;
+        std::string cmd = "setaudio " + path_ + " volume to " + std::to_string(volume_);
+        mciSendString(cmd.c_str(), NULL, 0, NULL);
+    };
+    void close() {
+        std::string cmd = "close " + path_;
+        mciSendString(cmd.c_str(), NULL, 0, NULL);
+    };
+};
+
 class ui {
 private:
-    ExMessage msg_;              // ��Ϣ
-    std::list<button> buttons_;  // ��ť�б�
-    std::list<image> images_;    // ͼƬ�б�
+    ExMessage msg_;
+    std::list<image> images_;
+    std::list<button> buttons_;
+    
 public:
-    // ����һ������
     void InitUI(std::string title, int width, int height) {
-        initgraph(width, height, EX_SHOWCONSOLE);  // EX_SHOWCONSOLE
+        initgraph(width, height);// EX_SHOWCONSOLE
         HWND hwnd = GetHWnd();
         SetWindowText(hwnd, title.c_str());
     };
-    // ��ʼ��ͼ
     void begin() { BeginBatchDraw(); };
-    // ������ͼ
     void end() { EndBatchDraw(); };
-    // �ñ���ɫ��ս���
     void clear() { cleardevice(); };
-    // ����һ��ͼƬ
     image AddImage(std::string path, int x = 0, int y = 0) {
         image img(path, x, y);
         images_.push_back(img);
         return img;
     };
-    // ��ӡ�ض���ͼƬ
     void PrintImage(int index = 0) {
         int i = 0;
         for (auto x : images_) {
@@ -89,19 +118,16 @@ public:
             i++;
         }
     };
-    // ����һ����ť
     button AddButton(int x, int y, int w, int h, std::string text = "", int line_color = WHITE, bool solid = 0, int full_color = RED) {
         button b(x, y, w, h, text, line_color, solid, full_color);
         buttons_.push_back(b);
         return b;
     };
-    // ��ӡ��ť
     void PrintButton() {
         for (auto x : buttons_) {
             x.draw();
         }
     };
-    // ��ⰴť�б����Ƿ��а�ť����������ر�����İ�ť���±�
     int CheckButton() {
         if (peekmessage(&msg_, PM_REMOVE)) {
             if (msg_.message == WM_LBUTTONDOWN) {
